0cbd2ad750b757ad4caab057e9260148138890e4
Cleanly <oskar.turesson99@gmail.com>
2021-05-01 21:48:34 +0200

    updated some configuration

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,17 @@
-import postcss from 'rollup-plugin-postcss'
-import autoprefixer from 'autoprefixer'
+import postcss from "rollup-plugin-postcss";
+import autoprefixer from "autoprefixer";
+import normalize from "postcss-normalize";
 
 export default {
-	input: 'src/scripts/index.js',
+	input: "src/scripts/index.js",
 	output: {
-	  file: 'dist/bundle.js',
-	  format: 'iife'
+		file: "dist/bundle.js",
+		format: "iife",
 	},
 	plugins: [
 		postcss({
-		  extract: 'main.css',
-		  plugins: [autoprefixer]
+			extract: "main.css",
+			plugins: [autoprefixer, normalize],
 		}),
-	  ]
-  };
-
+	],
+};
