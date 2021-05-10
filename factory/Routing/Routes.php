d4de4c4759891fe934960709f80c1c5360f2f0e6
Cleanly <oskar.turesson99@gmail.com>
2021-05-10 22:17:00 +0200

    added documentation for class

--- factory/Routing/Routes.php
+++ factory/Routing/Routes.php
@@ -1,10 +1,25 @@
 <?php
 
+/**
+ * @author Cleanly1 
+ */
 class Routes {
 
-	protected $needle;
-	protected $dir;
-	protected $prefix;
+	/**
+	 * The seperator for views/pages files
+	 * @var string 
+	 */
+	protected string $needle;
+	/**
+	 * Name of the directory where your views/pages
+	 * @var string
+	 */
+	protected string $dir;
+	/**
+	 * The prefix needed to get to the root of the project to find the views directory
+	 * @var string
+	 */
+	protected string $prefix;
 
 	function __construct(string $needle, string $dir, string $dirPrefix) {
 		$this->needle = $needle;
@@ -12,6 +27,11 @@ class Routes {
 		$this->prefix = $dirPrefix;
 	}
 
+	/**
+	 * Gets all the routes from the views/page directory
+	 *
+	 * @return string[]
+	 */
 	function getRoutes() {
 		$array = array_filter(scandir(__DIR__ . $this->prefix . $this->dir), function ($file) {
 			return str_contains($file, $this->needle);
